0e1d7c445e58e47f65ed3a569a905035fce4b16f
Atlas <atlas@atlas-systems.uk>
2026-08-13 02:40:19 +0100

    feat(spectral-forge): link audio flagship counterpart

--- static/js/spectral-forge/app.js
+++ static/js/spectral-forge/app.js
@@ -1,3 +1,6 @@
 "use strict";
 
+import { installFlagshipCounterpart } from "/lab/shared/flagship-counterparts.js?v=20260813-spectral-forge";
 import "./app-core.js";
+
+installFlagshipCounterpart();
