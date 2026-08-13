f1d32868305abd5a7020fa03820b0db71030b2a9
Atlas <atlas@atlas-systems.uk>
2026-08-13 14:11:15 +0100

    feat(spectral-forge): activate Field 3.0 renderer

--- static/js/spectral-forge/app.js
+++ static/js/spectral-forge/app.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import { installFlagshipCounterpart } from "/lab/shared/flagship-counterparts.js?v=20260813-atlas-audio-family-v1";
+import "./spectral-field-art.js?v=20260813-field-3";
 import "./app-core.js";
 
 const counterpartRail = installFlagshipCounterpart();
