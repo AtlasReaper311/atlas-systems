6723f2122fd47e9f8c5181c86dc8b8e17ce439be
Atlas <atlas@atlas-systems.uk>
2026-08-14 02:49:29 +0100

    fix(forge): version playback renderer runtime

--- static/js/spectral-forge/spectral-field-art.js
+++ static/js/spectral-forge/spectral-field-art.js
@@ -2,7 +2,7 @@
 
 import { installSpectralFieldRuntime } from "./spectral-field-install.js";
 import { draw } from "./spectral-field-compose-v4.js";
-import { syncLoop, tick, transitionActive, transitionMix, updateAccessibleSummary } from "./spectral-field-runtime.js";
+import { syncLoop, tick, transitionActive, transitionMix, updateAccessibleSummary } from "./spectral-field-runtime.js?v=20260814-field-4-playback";
 import { drawAfterimages, drawBackdrop, drawCausalPropagation, drawFracture, drawLattice, drawMicrostructure, drawPulseEmissions, drawSelectedRoute, drawSignalFilaments, drawSpectralBody } from "./spectral-field-layers-v4.js";
 
 installSpectralFieldRuntime({
